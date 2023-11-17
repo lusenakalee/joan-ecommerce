d8d9fdb71c02dbd55342ea651a3352d2c92bc387
lusenakalee <lusenakalee@gmail.com>
2023-11-17 15:10:49 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Header from "../components/Header.js";
+import Banner from "../components/Banner.js";
+import ProductFeed from "../components/ProductFeed.js";
+
+export default function Home({ products }) {
+  return (
+    <div>
+      <Head>
+        <title>ELEGANT</title>
+      </Head>
+      <Header/>
+      <main className="max-w-screen-2xl mx-auto">
+      <Banner/>
+      <ProductFeed products={products} />
+      
+      </main>
+
+    
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products")
+     .then(res => res.json())
+
+  return {
+     props: {
+        products
+     }
+  }
+}
