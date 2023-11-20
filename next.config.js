cda4a31e76bec638f80f9f3fc7b866f9592bab0d
lusenakalee <lusenakalee@gmail.com>
2023-11-20 08:10:10 +0300

    checkout session continuation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images:{
-       domains:[ "fakestoreapi.com"]
+       domains:[ "fakestoreapi.com" , "elegancetest.onrender.com"]
     },
     env: {
       stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
