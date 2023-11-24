51c3b22f158944053be1815b062854dab9721fd3
lusenakalee <lusenakalee@gmail.com>
2023-11-24 11:58:13 +0300

    checkout prod

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -47,7 +47,11 @@ function CheckoutProduct({
 
     return (
         <div className='grid grid-cols-5'>
-            <img src={image} width={200} height={200} objectFit='contain' />
+            <img 
+                src={`https://elegancetest.onrender.com/static/uploads/${image}`}            
+                width={200} 
+                height={200} 
+                objectFit='contain' />
 
             <div className='col-span-3 mx-5'>
                 <p>{title}</p>
